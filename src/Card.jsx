c51f437c79e1d78a3585ac4f39c7028c5ba0225d
Vansh-Rajput <vanshrjpt8@gmail.com>
2025-08-02 20:31:45 +0530

    changed

--- src/Card.jsx
+++ src/Card.jsx
@@ -28,12 +28,12 @@ const Card = ({ feed_data,setover }) => {
          //work evenif swipe was slow
       onSwipe={(direction)=>handleSwipe(direction,_id)} //inbuilt
       preventSwipe={['up', 'down']}
-      className="absolute cursor-grab"
+      className="absolute "
     >
       <div className="w-[400px] h-min bg-gray-900 border border-gray-700 rounded-lg p-2.5 shadow-xl shadow-gray-800">
         <img className="rounded-lg h-[460px] object-cover pointer-events-none" src={photourl} />
-        <div className="p-3 flex flex-col gap-2">
-          <h5 className="mb-2 text-2xl font-bold text-white flex items-center justify-between">
+        <div className="p-3 flex flex-col">
+          <h5 className="mb-3 text-2xl font-bold text-white flex items-center justify-between">
             {first_name.toUpperCase()} {last_name.toUpperCase()}
           
      { ispremium && <img src="/ChatGPT Image Jul 28, 2025, 10_12_54 PM.png" className='w-[50px] h-[50px]'/> }
@@ -41,13 +41,27 @@ const Card = ({ feed_data,setover }) => {
 
           <div className="w-[50%] bg-gray-700 h-1 rounded-full">
   <div
-    className="bg-green-500 h-1 rounded-full"
+    className="bg-blue-500 h-1 rounded-full"
         style={{ width: `${score}%` }}
   />
 </div>
 <p className="text-xs text-gray-300">{score}% skill match</p>
 
-          <p className="text-gray-400">{about}</p>
+          <p className="text-gray-400 mt-3 mb-1">{about}</p>
+
+
+    <div className="flex flex-wrap gap-2">
+  {skills.map((skill, idx) => (
+    <span
+      key={idx}
+      className="bg-gray-800 text-blue-400 text-xs font-medium px-2 py-1 rounded-full border border-blue-700"
+    >
+      {skill}
+    </span>
+
+  ))}
+</div>
+
         </div>
       </div>
     </TinderCard>
